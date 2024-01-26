afab0b3904904a27dcba5263c6d1de7213c89047
younessgt <youness.merzak123@gmail.com>
2024-01-26 16:05:57 +0100

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,3 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" script that contain index_range function """
 
-test
+
+def index_range(page, page_size):
+    """ function should return a tuple of size two containing a start index
+    and an end index corresponding to the range of indexes to return in a
+    list for those particular pagination parameters."""
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
