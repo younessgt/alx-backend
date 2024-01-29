a802a9c56a5ed2703081812344adb8ecd481d32a
younessgt <youness.merzak123@gmail.com>
2024-01-29 19:57:33 +0100

    task2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -51,23 +51,27 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """ returning a dictionary of pagination data"""
-        new_dict: Dict[Any, Any] = {}
         data_list = self.get_page(page, page_size)
 
-        new_dict['page_size'] = len(data_list)
-        new_dict['page'] = page
-        new_dict['data'] = data_list
-        new_dict['total_pages'] = math.ceil(len(self.dataset()) / page_size)
+        page_size = len(data_list)
+        data = data_list
+        total_pages = math.ceil(len(self.dataset()) / page_size)
 
         if len(data_list) == 0:
-            new_dict['next_page'] = None
+            next_page = None
         else:
-            new_dict['next_page'] = page + 1
+            next_page = page + 1
 
         if (page == 1):
-            new_dict['prev_page'] = None
+            prev_page = None
         else:
-            new_dict['prev_page'] = page - 1
+            prev_page = page - 1
 
-        new_dict['total_pages'] = math.ceil(len(self.dataset()) / page_size)
-        return new_dict
+        return {
+                'page_size': page_size,
+                'page': page,
+                'data': data,
+                'next_page': next_page,
+                'prev_page': prev_page,
+                'total_pages': total_pages
+                }
