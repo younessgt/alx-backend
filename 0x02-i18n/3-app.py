a990bc83e68fffa8e2b2384cf024c6acafb54e0d
younessgt <youness.merzak123@gmail.com>
2024-02-06 15:41:27 +0100

    task3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,1 +1,42 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" script with a single route that output
+Welcome to Holberton” as page title """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ flask app config """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ function that return the best match
+    of our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+home_title = gettext("Welcome to Holberton")
+home_header = gettext('Hello world!')
+
+
+@app.route('/')
+def welcome():
+    """ returning page '2-index.html '"""
+    return render_template('2-index.html',
+                           home_title=home_title,
+                           home_header=home_header)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
