2d93dbccbae64bdec6a2354047c9fd66d92abff8
younessgt <youness.merzak123@gmail.com>
2024-02-06 13:47:35 +0100

    task0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,9 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     """ returning welcome to Holberton"""
-    return render_template('0-index.html',
-                           title="Welcome to Holberton",
-                           header="Hello world")
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
