473e9954c07c02db0337efa38c61a5b4b4a2c179
younessgt <youness.merzak123@gmail.com>
2024-02-06 13:40:30 +0100

    task0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" script with a single route that output
+Welcome to Holberton” as page title """
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    """ returning welcome to Holberton"""
+    return render_template('0-index.html',
+                           title="Welcome to Holberton",
+                           header="Hello world")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
